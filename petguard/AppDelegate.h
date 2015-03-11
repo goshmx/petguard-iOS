a957d002b7f43b31692af3d8d00c3dad6059651a
TRON <osvaldocube@gmail.com>
2015-03-11 03:22:32 -0600

    Initial Commit

---
+++ petguard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  petguard
+//
+//  Created by TRON on 11/03/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ petguard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  petguard
+//
+//  Created by TRON on 11/03/15.
+//  Copyright (c) 2015 TRON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
